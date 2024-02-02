aafd03f22fba3825b9b716fdf04723ac9ef2001f
myradio <154856702+pristaaaa@users.noreply.github.com>
2024-02-02 20:36:28 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+jwplayer("video").setup({
+  width: "100%",
+  height: "100%",
+  autostart: true,
+  mute: false,
+  file: "https://prod-ent-live-gm.jiocinema.com/hls/live/2100299/hd_akamai_androidmob_avc_eng_indvseng_test02day01020224/master.m3u8"
+});
