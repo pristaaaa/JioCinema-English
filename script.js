cef34a78268745498969b6b5df6a06c9e3d21389
myradio <154856702+pristaaaa@users.noreply.github.com>
2024-02-24 08:59:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ jwplayer("video").setup({
   height: "100%",
   autostart: true,
   mute: false,
-  file: "http://prod-ent-live-gm.jiocinema.com/hls/live/2100304/uhd_akamai_ctv_avc_eng_indvseng_test04day02240224/master.m3u8"
+  file: "https://prod-ent-live-gm.jiocinema.com/hls/live/2100304/uhd_akamai_ctv_avc_eng_indvseng_test04day02240224/master.m3u8"
 });
